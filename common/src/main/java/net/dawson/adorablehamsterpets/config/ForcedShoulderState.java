e22a2dc19df28ebfe1932789a489c6b047550e16
The Scarlet Fox <tweekythefox@gmail.com>
2025-12-10 09:30:40 -0600

    [FIX] Localize config enums, conditions, and group descriptions for translation support

--- common/src/main/java/net/dawson/adorablehamsterpets/config/ForcedShoulderState.java
+++ common/src/main/java/net/dawson/adorablehamsterpets/config/ForcedShoulderState.java
@@ -1,7 +1,16 @@
 package net.dawson.adorablehamsterpets.config;
 
-public enum ForcedShoulderState {
+import me.fzzyhmstrs.fzzy_config.util.EnumTranslatable;
+import org.jetbrains.annotations.NotNull;
+
+public enum ForcedShoulderState implements EnumTranslatable {
     ALWAYS_STAND,
     ALWAYS_SIT,
-    ALWAYS_LAY_DOWN
+    ALWAYS_LAY_DOWN;
+
+    @NotNull
+    @Override
+    public String prefix() {
+        return "config.adorablehamsterpets.enum.forced_shoulder_state";
+    }
 }
