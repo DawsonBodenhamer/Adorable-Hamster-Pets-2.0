1075a830df55176f314c03604d85ffa341672d76
The Scarlet Fox <tweekythefox@gmail.com>
2025-12-10 09:30:40 -0600

    [FIX] Localize config enums, conditions, and group descriptions for translation support

--- common/src/main/java/net/dawson/adorablehamsterpets/config/DismountOrder.java
+++ common/src/main/java/net/dawson/adorablehamsterpets/config/DismountOrder.java
@@ -1,6 +1,15 @@
 package net.dawson.adorablehamsterpets.config;
 
-public enum DismountOrder {
+import me.fzzyhmstrs.fzzy_config.util.EnumTranslatable;
+import org.jetbrains.annotations.NotNull;
+
+public enum DismountOrder implements EnumTranslatable {
     LIFO, // Last-In, First-Out
-    FIFO  // First-In, First-Out
+    FIFO; // First-In, First-Out
+
+    @NotNull
+    @Override
+    public String prefix() {
+        return "config.adorablehamsterpets.enum.dismount_order";
+    }
 }
