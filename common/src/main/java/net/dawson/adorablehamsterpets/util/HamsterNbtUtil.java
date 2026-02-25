ad3690f5807352628c6c0b8f74404d95f4ca2e7b
The Scarlet Fox <tweekythefox@gmail.com>
2026-02-25 15:50:10 -0600

    [FIX] Reconcile accessory state when reading NBT

--- common/src/main/java/net/dawson/adorablehamsterpets/util/HamsterNbtUtil.java
+++ common/src/main/java/net/dawson/adorablehamsterpets/util/HamsterNbtUtil.java
@@ -216,6 +216,9 @@ public final class HamsterNbtUtil {
         // --- 8. Read Flight Data ---
         hamster.setHasPlayedIncomingSound(nbt.getBoolean("HasPlayedIncomingSound"));
 
+        // --- 9. Reconcile Accessory State ---
+        hamster.updateAccessoryState();
+
         hamster.setLoadingNbt(false);
     }
 
@@ -352,6 +355,9 @@ public final class HamsterNbtUtil {
             // --- 7. Reset Transient Action Flags ---
             hamster.setHamsterFlag(HamsterEntity.CLEANING_FLAG, false);
             hamster.setDozingPhase(HamsterEntity.DozingPhase.NONE);
+
+            // --- 8. Reconcile Accessory State ---
+            hamster.updateAccessoryState();
         }
         return hamster;
     }
