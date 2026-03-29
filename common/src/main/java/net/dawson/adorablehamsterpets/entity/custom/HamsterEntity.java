544b8f39bdeeb5670800ff2467db610a1cca0b86
The Scarlet Fox <tweekythefox@gmail.com>
2026-03-29 17:51:08 -0500

    [FIX] Change hazard pathfinding penalties to -1.0F to strictly prevent suicidal fleeing

--- common/src/main/java/net/dawson/adorablehamsterpets/entity/custom/HamsterEntity.java
+++ common/src/main/java/net/dawson/adorablehamsterpets/entity/custom/HamsterEntity.java
@@ -445,9 +445,9 @@ public class HamsterEntity extends TameableEntity implements GeoEntity, Implemen
 
         // --- Set pathfinding penalties for all relevant goals ---
         this.setPathfindingPenalty(PathNodeType.WATER, 16.0F);
-        this.setPathfindingPenalty(PathNodeType.LAVA, 16.0F);
-        this.setPathfindingPenalty(PathNodeType.DAMAGE_FIRE, 16.0F);
-        this.setPathfindingPenalty(PathNodeType.DANGER_FIRE, 16.0F);
+        this.setPathfindingPenalty(PathNodeType.LAVA, -1.0F);
+        this.setPathfindingPenalty(PathNodeType.DAMAGE_FIRE, -1.0F);
+        this.setPathfindingPenalty(PathNodeType.DANGER_FIRE, -1.0F);
     }
 
 
